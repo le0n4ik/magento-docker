a32661d41b76189db8d2c8e16e68c5cd68b1c5ce
Leonid Poluianov <poluyano@adobe.com>
2021-07-14 09:44:06 -0500

    Install magento with data-exporter services

--- reinstall-magento.sh
+++ reinstall-magento.sh
@@ -17,5 +17,5 @@ cd $DOCKER_PROJECT_DIR
 
 
 # composer based installation
-docker-compose exec app magento reinstall_monolith
+docker-compose exec app magento reinstall_saas
 docker-compose exec app magento config_setup
